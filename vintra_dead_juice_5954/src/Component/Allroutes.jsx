087cff1b009b2f0cb921608b83c3157511f5eaa9
Vishal Giri <vishalgiri197@gmail.com>
2023-03-30 23:08:41 +0530

    product_and_filtering

--- vintra_dead_juice_5954/src/Component/Allroutes.jsx
+++ vintra_dead_juice_5954/src/Component/Allroutes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from "../Pages/Home"
+import Product from '../Pages/Product';
  export default function AllRoutes(){
    return <Routes>
      
    <Route path="/" element={<Home/>}></Route>
-   <Route path="/products" element={<h1 style={{margintop:"300px"}}>product</h1>}>product</Route>
+   <Route path="/products" element={<Product/>}></Route>
    <Route path="/products/:id" element={<h1>home</h1>}></Route>
 
    <Route path="/cart/:id" element={<h1>home</h1>}></Route>
